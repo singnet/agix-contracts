e6f1ce20220bfc52c54b5a4bf515223d11bc296a
Sridhar Babu Kolapalli <ksridharbabuus@gmail.com>
2021-04-25 12:34:08 +0530

    Updated the Token Symbol

--- migrations/2_SingularityNetToken.js
+++ migrations/2_SingularityNetToken.js
@@ -1,7 +1,7 @@
 let SingularityNetToken = artifacts.require("./SingularityNetToken.sol");
 
 const name = "SingularityNET Token"
-const symbol = "AGI"
+const symbol = "AGIV2"
 
 module.exports = function (deployer) {
     deployer.deploy(SingularityNetToken, name, symbol);
